c296bce465d4e160ada5d216a979afbc7b622cdb
Andrew Volk <andrewvolk@Andrews-MacBook-Air.local>
2024-05-06 03:07:23 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+function openRegistration() {
+  document.getElementById("login").style.display = "none";
+  document.getElementById("registration").style.display = "flex";
+}
+
+function openLogin() {
+  document.getElementById("login").style.display = "flex";
+  document.getElementById("registration").style.display = "none";
+}
